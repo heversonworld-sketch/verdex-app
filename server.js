66f412c7d0fe92ef43ae9a1507648b96abd26dbf
VerdexDev <verdex@termux.dev>
2025-12-20 17:50:06 -0300

    Frontend CORRIGIDO

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-app.get("/", (req,res)=>res.sendFile(__dirname+"/index.html"));
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:green;font-size:50px;text-align:center">🚀 VERDEX Frontend 1000+ linhas VIVO!</h1>');
+});
+
+app.listen(process.env.PORT || 10000, () => {
+  console.log('🚀 VERDEX Frontend VIVO!');
+});
